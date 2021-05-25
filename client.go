59f96c7c5f4c27abee8e87fa9dc97ec996fe4710
du-research2 <you@example.com>
2021-05-25 12:10:57 +0000

    first commit

---
+++ client.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "github.com/du-research2/version"
+)
+
+func main() {
+    // Get a greeting message and print it.
+    message := version.Hello("Gladys")
+    fmt.Println(message)
+}
+
+
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/du-research2b/client
+
+go 1.16
+
+require github.com/du-research2/version v1.0.0
